e71559e790ffd1a1ffc8c1db8ea0651848780896
abfiker <new94abemelek@gmail.com>
2025-01-31 22:33:03 +0300

    ticket updated listener added to order service

--- orders/src/events/listener/ticket-created-listener.ts
+++ orders/src/events/listener/ticket-created-listener.ts
@@ -1,6 +1,5 @@
 import { Listener, Subjects, TicketCreatedEvent } from '@abticketing/common';
 import { Message } from 'node-nats-streaming';
-import { natsWrapper } from '../../nats-wrapper';
 import { Ticket } from '../../models/ticket';
 import { queueGroupName } from './queue-group-name';
 
---
+++ orders/src/events/listener/ticket-updated-listener.ts
@@ -0,0 +1,25 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Listener,
+  NotFoundError,
+  Subjects,
+  TicketUpdateEvent,
+} from '@abticketing/common';
+import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
+export class TicketUpdatedListener extends Listener<TicketUpdateEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: TicketUpdateEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    const { title, price } = data;
+    ticket.set({ title, price });
+
+    await ticket.save();
+    // acknowledge the message
+    msg.ack();
+  }
+}
