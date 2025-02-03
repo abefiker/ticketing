bf64335bd2fde5446561028142634249b31dcdbb
abfiker <new94abemelek@gmail.com>
2025-02-03 07:15:10 +0300

    creating order model inside payments service

---
+++ payments/src/models/order.ts
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@abticketing21/common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+interface OrderAttrs {
+  id: string;
+  status: OrderStatus;
+  userId: string;
+  price: number;
+  version: number;
+}
+interface OrderDoc extends mongoose.Document<any> {
+  status: OrderStatus;
+  userId: string;
+  price: number;
+  version: number;
+}
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+const orderSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    status: attrs.status,
+    userId: attrs.userId,
+    price: attrs.price,
+    version:attrs.version
+  });
+};
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+export { Order };
