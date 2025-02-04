ab45270fb0d58f16dff8cfa3c8984f1c3e6dc7d6
abfiker <new94abemelek@gmail.com>
2025-02-04 14:40:35 +0300

    adding expiration time

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,23 @@
-const OrderShow = () => {
-  return;
-  <div>
-    <h1>Order Show</h1>
-  </div>;
+import { useEffect, useState } from 'react';
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState();
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expireAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  return <div>Time left to pay : {timeLeft} seconds</div>;
+};
+OrderShow.getIntialProps = async (client, context) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/orders/${orderId}`);
+  return { order: data };
 };
 export default OrderShow;
